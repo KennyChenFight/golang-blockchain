90fefc3dff566e89ab00f73489cf930f7a017b6b
KennyChen <kennychen851228@gmail.com>
2019-10-02 00:57:41 +0800

    section 2

---
+++ blockchain/block.go
@@ -0,0 +1,37 @@
+package blockchain
+
+type BlockChain struct {
+	Blocks []*Block
+}
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+	Nonce    int
+}
+
+func CreateBlock(data string, pervHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), pervHash, 0}
+	pow := NewProof(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
+	return block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	pervBlock := chain.Blocks[len(chain.Blocks)-1]
+	new := CreateBlock(data, pervBlock.Hash)
+	chain.Blocks = append(chain.Blocks, new)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
+}
